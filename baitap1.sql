d228b1de8adef390b4411ac7c4bec477f905d58a
Huong14 <thienhuongbluesky14@gmail.com>
2026-05-04 15:27:44 +0900

    Add files via upload

---
+++ baitap1.sql
@@ -0,0 +1,16 @@
+INSERT INTO student_constrainter(`Mã sinh  viên`,`Họ và tên`,`Email`,`Tuổi`)
+VALUES
+(1,'Tran Van A','tva@gmail.com','19'),
+(2,'Tran Thanh D','ttd@gmail.com','21'),
+(3,'Nguyen Thi C','ntc@gmail.com','20'),
+(4,'Hua Van Dat','hvd@gmail.com','21'),
+(5,'Do Thi Linh',NULL,'19');
+
+SELECT * FROM student_constrainter;
+
+SELECT `Mã sinh viên`,`Họ và tên`,`Email` FROM student_constrainter;
+
+
+
+
+SELECT * FROM stu_sql.student_constrainter;
